8ff8996c1409eb11333c8d9c5fb334f1e930fdd0
Jorge Fandinno <jorgefandinno@gmail.com>
2025-07-29 21:05:15 +0200

    update

--- fasp/ast/rewriting.py
+++ fasp/ast/rewriting.py
@@ -72,7 +72,7 @@ class NormalForm2PredicateTransformer:
             ),
         )
 
-    def rewrite(self, node, *args, **kwargs) -> AST:
+    def rewrite(self, node, *args, **kwargs) -> AST | None:
         result = self._dispatch(node, *args, **kwargs)
         if not result:
             return node
