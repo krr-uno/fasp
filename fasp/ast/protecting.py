c6a39838394df722de1f493d546d5937b009c2bf
Jorge Fandinno <jorgefandinno@gmail.com>
2025-08-02 15:31:30 +0200

    update

---
+++ fasp/ast/protecting.py
@@ -0,0 +1,132 @@
+from functools import singledispatchmethod
+from inspect import signature
+from itertools import chain
+import re
+from typing import AbstractSet, Any, Iterable, cast
+from click import Argument
+from clingo import ast
+from clingo.core import Location, Position, Library
+from clingo.symbol import Number
+
+from fasp.util.ast import AST, StatementAST
+
+INT_TO_SIGN = [
+    ast.Sign.NoSign,
+    ast.Sign.Single,
+    ast.Sign.Double,
+]
+
+INT_TO_RELATION = [
+    ast.Relation.Equal,
+    ast.Relation.Greater,
+    ast.Relation.GreaterEqual,
+    ast.Relation.Less,
+    ast.Relation.LessEqual,
+    ast.Relation.NotEqual,
+]
+
+SIGN_TO_INT = {r: i for i, r in enumerate(INT_TO_SIGN)}
+
+RELATION_TO_INT = {r: i for i, r in enumerate(INT_TO_RELATION)}
+
+
+class ComparisonProtector:
+    """
+    A class to protect comparisons in a Clingo AST.
+    """
+
+    def __init__(self, library: Library, comparison_name: str = "Comparison", guard_name: str = "Guard"):
+        self.library = library
+        self.comparison_name = comparison_name
+        self.guard_name = guard_name
+        position = Position(library, "<aux>", 0, 0)
+        location = Location(position, position)
+        self.sign_to_int = {
+            r: ast.TermSymbolic(library, location, Number(library, n))
+            for r, n in SIGN_TO_INT.items()
+        }
+        self.relation_to_int = {
+            r: ast.TermSymbolic(library, location, Number(library, n))
+            for r, n in RELATION_TO_INT.items()
+        }
+
+    def _guard_to_function(
+        self, location: Location, guard: ast.RightGuard
+    ) -> ast.TermFunction:
+        arguments = ast.ArgumentTuple(
+            self.library, [self.relation_to_int[guard.relation], guard.term]
+        )
+        return ast.TermFunction(self.library, location, self.guard_name, [arguments])
+
+    def protect_comparison(
+        self, comparison: ast.LiteralComparison
+    ) -> ast.LiteralSymbolic:
+        """
+        Rewrites a LiteralComparison as a positive LiteralSymbolic of the form
+            Comparison(left, right, sign)
+        where right is a tuple of functions symbols Guard(operator, term)
+        """
+        location = comparison.location
+        sign = self.sign_to_int[comparison.sign]
+        left = comparison.left
+        right = ast.TermTuple(
+            self.library,
+            location,
+            [ast.ArgumentTuple(self.library, [self._guard_to_function(location, g) for g in comparison.right])],
+        )
+        atom = ast.TermFunction(
+            self.library,
+            location,
+            self.comparison_name,
+            [ast.ArgumentTuple(self.library, [left, right, sign])],
+        )
+        return ast.LiteralSymbolic(self.library, location, ast.Sign.NoSign, atom)
+
+    def __call__(self, comparison: ast.LiteralComparison) -> ast.LiteralSymbolic:
+        """
+        Call method to protect a comparison.
+        """
+        return self.protect_comparison(comparison)
+
+
+class _ComparisonProtectorTransformer:
+    """
+    A transformer to protect comparisons in a Clingo AST.
+    """
+
+    def __init__(self, library: Library):
+        self.library = library
+        self.protect_comparison = ComparisonProtector(library)
+
+    @singledispatchmethod
+    def dispatch(self, node: AST) -> AST:
+        return node.transform(self.library, self.dispatch)
+
+    @dispatch.register
+    def _(self, node: ast.LiteralComparison) -> ast.LiteralSymbolic:
+        return self.protect_comparison(node)
+
+    @dispatch.register
+    def _(
+        self, node: ast.LiteralBoolean | ast.LiteralSymbolic
+    ) -> ast.LiteralBoolean | ast.LiteralSymbolic:
+        return node
+    
+    def rewrite(self, node: AST) -> AST:
+        if not isinstance(node, ast.StatementRule):
+            return node
+        return node.transform(self.library, self.dispatch)
+
+
+def protect_comparisons(library: Library, statements: Iterable[AST]) -> Iterable[AST]:
+    """
+    Protect comparisons in a Clingo AST.
+
+    Args:
+        statements (Iterable[AST]): The AST statements to protect.
+
+    Returns:
+        Iterable[AST]: The protected AST statements.
+    """
+    transformer = _ComparisonProtectorTransformer(library)
+    return (transformer.rewrite(statement) for statement in statements)
---
+++ tests/ast/test_protection.py
@@ -0,0 +1,90 @@
+from math import exp
+import textwrap
+from turtle import st
+import unittest
+
+from clingo import ast
+from clingo.core import Library
+from clingo.ast import RewriteContext, rewrite_statement
+from fasp.util.ast import AST
+
+from fasp.ast.protecting import protect_comparisons
+
+
+class TestSyntacticChecker(unittest.TestCase):
+    """
+    Test class for the syntactic checker.
+    """
+
+    def setUp(self):
+        """
+        Set up the test case with a library instance.
+        """
+        self.lib = Library()
+        self.rewrite_context = RewriteContext(self.lib)
+
+    def assertEqualRewrite(self, program, expected):
+        """
+        Helper method to assert that the syntactic checker finds the expected errors.
+
+        Args:
+            program (str): The program to check.
+            expected_errors (list): The list of expected SyntacticError instances.
+        """
+
+        statements = []
+
+        def callback(statement):
+            statements.append(statement)
+
+        ast.parse_string(self.lib, program, callback)
+
+        result = protect_comparisons(self.lib, statements)
+
+        expected_lines = [line.strip() for line in expected.splitlines()]
+
+        self.maxDiff = None
+        self.assertCountEqual(
+            list(map(lambda x: str(x).strip(), result)), expected_lines
+        )
+        for statement in result:
+            self.assertIsInstance(statement, AST)
+
+    def test_correct(self):
+        """Test syntax checking with a correct program snippet."""
+        program = """\
+            f = 100 :- not g = 100.
+            f(X) > 100 :- 1000 < g(X,Y) < 1010.
+            f(a,5) >= 100 :- g("string") <= 100, not h(3+5) != 300, not not z = p.
+        """
+        expected = textwrap.dedent(
+            """\
+            #program base.
+            Comparison(f,(Guard(0,100),),0) :- Comparison(g,(Guard(0,100),),1).
+            Comparison(f(X),(Guard(1,100),),0) :- Comparison(1000,(Guard(3,g(X,Y)),Guard(3,1010)),0).
+            Comparison(f(a,5),(Guard(2,100),),0) :- Comparison(g("string"),(Guard(4,100),),0); Comparison(h(3+5),(Guard(5,300),),1); Comparison(z,(Guard(0,p),),2).
+        """
+        ).strip()
+        self.assertEqualRewrite(program, expected)
+
+    # def test_symbol_argument(self):
+    #     """Test syntax checking with a correct program snippet."""
+    #     program = """\
+    #         f0 = X :- X=1; g=h.
+    #         f1(a) = X :- X=1; not g=h.
+    #         f1(5) = X :- X=1; not g=h.
+    #         f2(a,b) = X :- X=1; not g=h.
+    #     """
+    #     expected = textwrap.dedent(
+    #         """\
+    #         #program base.
+    #         Ff0(X) :- X=1; g=h.
+    #         Ff1(a,X) :- X=1; not g=h.
+    #         Ff1(5,X) :- X=1; not g=h.
+    #         Ff2(a,b,X) :- X=1; not g=h.
+    #         :- Ff0(_); 1 > #count { V: Ff0(V) }.
+    #         :- Ff1(X0,_); 1 > #count { V: Ff1(X0,V) }.
+    #         :- Ff2(X0,X1,_); 1 > #count { V: Ff2(X0,X1,V) }.
+    #     """
+    #     ).strip()
+    #     self.assertEqualRewrite(program, expected)
