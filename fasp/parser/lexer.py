804cf898085dc5481026c6cdad8c7117cc6a6abf
Jorge Fandinno <jorgefandinno@gmail.com>
2025-08-27 09:20:48 -0500

    starting parser

---
+++ fasp/parser/lexer.py
@@ -0,0 +1,86 @@
+import re
+from typing import NamedTuple
+
+from click import STRING
+
+OTHER = -1
+INTERVAL = 0
+DOT = 1
+RULEOP = 2
+NEWLINE = 3
+ASSIGN = 4
+LBRACER = 5
+RBRACER = 6
+HASHTAG = 7
+STRING = 8
+UNCLOSED_STRING = 9
+
+
+class Token(NamedTuple):
+    type: int
+    value: str
+    line: int
+    column: int
+
+
+UNESCAPED_STRING = r'[^"\\]+'
+ESCAPE_SEQUENCE = (
+    r"\\(?:[^xu0-7]|[0-7]{1,3}|x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|u\{[0-9a-fA-F]+\})"
+)
+STRING = rf'"(?:{UNESCAPED_STRING}|{ESCAPE_SEQUENCE})*"'
+
+TOKEN_SPECIFICATION = [
+    ("INTERVAL", r".."),  # Interval operator
+    ("DOT", r"."),  # Statement terminator
+    ("RULEOP", r":-"),  # Statement terminator
+    ("NEWLINE", "\n"),  # Line endings
+    ("ASSIGN", r":="),  # Assignment operator
+    ("LBRACER", r"{"),
+    ("RBRACER", r"}"),
+    ("HASHTAG", r"#"),
+    ("STRING", STRING),  # String literal
+    ("UNCLOSED_STRING", r'"'),
+]
+
+TOKEN_MAP = {name: i for i, (name, _) in enumerate(TOKEN_SPECIFICATION)}
+
+TOKEN_SPECIFICATION = [
+    (n, re.escape(r) if n in {"STRING", "NEWLINE"} else r)
+    for n, r in TOKEN_SPECIFICATION
+]
+
+TOKEN_REGEX = "|".join("(?P<%s>%s)" % pair for pair in TOKEN_SPECIFICATION)
+
+TOKEN_PATTERN = re.compile(TOKEN_REGEX)
+
+
+def tokenize(program: str):
+    line_num = 1
+    line_start = 0
+    for match in TOKEN_PATTERN.finditer(program):
+        kind = match.lastgroup
+        value = match.group()
+        column = match.start() - line_start
+        if kind == "NEWLINE":
+            line_start = match.end()
+            line_num += 1
+        yield Token(kind, value, line_num, column)
+
+
+for match in tokenize(
+    """\
+a :- b.
+c d..
+"""
+):
+    print(match)
+print("=" * 10)
+# for match in tokenize('"a".'):
+#     print(match)
+# print("="*10)
+# for match in tokenize('p("String", "Another string").'):
+#     print(match)
+# print("="*10)
+# for match in tokenize('p("String, "Another string").'):
+#     print(match)
+# print("="*10)
