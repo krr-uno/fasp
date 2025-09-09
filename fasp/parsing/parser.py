451d9c4d97384ae78cb63b2c4f02f1e0f7ea1024
Jorge Fandinno <jorgefandinno@gmail.com>
2025-09-09 11:44:57 -0500

    update

--- fasp/parsing/parser.py
+++ fasp/parsing/parser.py
@@ -1,55 +1,203 @@
-from collections.abc import Sequence
+from ast import unparse
+import ctypes
+import importlib
+
+import stat
+from typing import Iterable, Optional, Sequence
+from attr import dataclass
+from click import Path
+from tree_sitter import Language, Node, Query, QueryCursor, Tree, Parser
 
 from clingo import ast
-from clingo.core import Library
+from clingo.core import Library, Location, Position
 
-from fasp.parsing.lexer import UnParsedAssignmentRule, UnParsedClingoCode
-from fasp.util.ast import StatementAST, TermAST, parse_body
+from fasp.ast import AssignmentRule, HeadAggregateAssignment, HeadChoiceAssignment, HeadSimpleAssignment
+from fasp.util.ast import AST, TermAST
+
+def _load_ts_language(module: Optional[str], so_path: Optional[Path] = None) -> Language:
+    if module:
+        m = importlib.import_module(module)  # e.g., tree_sitter_clingo
+        cap = m.language()
+        return cap if isinstance(cap, Language) else Language(cap)
+    if so_path:
+        lib = ctypes.CDLL(str(so_path))
+        if not hasattr(lib, "tree_sitter_clingo"):
+            raise RuntimeError(f"{so_path} does not export tree_sitter_clingo")
+        lib.tree_sitter_clingo.restype = ctypes.c_void_p
+        ptr = lib.tree_sitter_clingo()
+        if not ptr:
+            raise RuntimeError("tree_sitter_clingo() returned NULL")
+        return Language(ptr)
+    raise SystemExit("Provide --module tree_sitter_clingo OR --so ./parser.(so|dylib|dll)")
 
 
-def _parse_clingo_block(
-    library: Library, block: UnParsedClingoCode
-) -> list[StatementAST]:
-    statements = []
-    ast.parse_string(ast.Library(), block.source, statements.append)
-    return statements
+
+def _ast_merge(asts1: Iterable[AST], asts2: Iterable[AST]) -> Iterable[AST]:
+    asts = []
+    asts1_i = iter(asts1)
+    asts2_i = iter(asts2)
+    ast1 = next(asts1_i, None)
+    ast2 = next(asts2_i, None)
+    while ast1 and ast2:
+        if ast1.location < ast2.location:
+            asts.append(ast1)
+            ast1 = next(asts1_i, None)
+        else:
+            asts.append(ast2)
+            ast2 = next(asts2_i, None)
+    while ast1:
+        asts.append(ast1)
+        ast1 = next(asts1_i, None)
+    while ast2:
+        asts.append(ast2)
+        ast2 = next(asts2_i, None)
+    return asts
 
 
-def _parse_assignment_right(library: Library, code: str) -> tuple[
-    TermAST | ast.BodyAggregate,
-    Sequence[ast.StatementComment],
-    Sequence[ast.StatementComment],
-]:
-    try:
-        body, pc, lt = parse_body(library, code)
-    except Exception:
-        raise ValueError(
-            f"Expected exactly one term or aggregate in assignment right-hand side of an assignment, found {code}"
+class TreeSitterParser:
+    """
+    A simple wrapper around the Tree-sitter parser.
+    """
+
+    def __init__(self, library: Library):
+        self.library = library
+        self.language = _load_ts_language("tree_sitter_clingo")
+        self.parser = Parser(self.language)
+        self.assignment_rule_query = Query(self.language, "(assignment_rule) @match")
+
+    def parse(self, src: str) -> AST:
+        """
+        Parse source code into an AST.
+        """
+        src_bytes = bytes(src, "utf8")
+        nodes = self._tree_parse_assignments(src_bytes)
+        assigment_rules = [self._parse_assignment_rule(n) for n in nodes]
+        src_array = bytearray(src_bytes)
+        for node in nodes:
+            for i in range(node.start_byte, node.end_byte):
+                src_array[i] = ord(b" ")
+        src_bytes = bytes(src_array)
+        src2 = src_bytes.decode("utf-8")
+        statements = []
+        ast.parse_string(self.library, src2, statements.append)
+        return _ast_merge(assigment_rules, statements[1:])
+
+
+    def _parse_assignment_rule(self, node: Node) -> AST:
+        children = node.children
+        unparsed_head = children[0]
+        if unparsed_head.type == "simple_assignment":
+            head = self._parse_simple_assignment(unparsed_head)
+        elif unparsed_head.type == "aggregate_assignment":
+            head = self._parse_aggregate_assignment(unparsed_head)
+        else:
+            head = self._parse_choice_assignment(unparsed_head)
+        if len(children) > 2:
+            unparse_body = children[2]
+            body = self._clingo_parse_body(unparse_body.text.decode("utf8"))
+        else:
+            body = []
+
+        return AssignmentRule(
+            self.library,
+            self._location_from_node(node),
+            head,
+            body,
         )
-    if len(body) != 1:
-        raise ValueError(
-            f"Expected exactly one term or aggregate in assignment right-hand side of an assignment, found {code}"
+    
+    def _preparse_assignment(self, node: Node) -> tuple[TermAST, str]:
+        unparsed_function = node.children[0].text.decode("utf-8")
+        unparsed_value = node.children[2].text.decode("utf-8")
+        assigned_function = ast.parse_term(self.library, unparsed_function)
+        return assigned_function, unparsed_value
+
+    def _parse_simple_assignment(self, node: Node) -> AST:
+        assigned_function, unparsed_value = self._preparse_assignment(node)
+        value = ast.parse_term(self.library, unparsed_value)
+        return HeadSimpleAssignment(
+            self.library,
+            self._location_from_node(node),
+            assigned_function,
+            value,
         )
-    right = body[0]
-    if isinstance(right, ast.BodySimpleLiteral) and isinstance(
-        right.literal, ast.LiteralSymbolic
-    ):
-        right = right.literal.atom
-    elif (
-        not isinstance(right, ast.BodyAggregate)
-        or right.left is not None
-        or right.right is not None
-    ):
-        raise ValueError(
-            f"Expected exactly one term or aggregate in assignment right-hand side of an assignment, found {code}"
+    
+    def _parse_aggregate_assignment(self, node: Node) -> AST:
+        assigned_function, unparsed_aggregate = self._preparse_assignment(node)
+        aggregate = self._clingo_parse_body_aggregate(unparsed_aggregate)
+        return HeadAggregateAssignment(
+            self.library,
+            self._location_from_node(node),
+            assigned_function,
+            aggregate.function,
+            aggregate.elements,
         )
-    return right, pc, lt
 
+    def _parse_choice_assignment(self, node: Node) -> AST:
+        assigned_function, unparsed_choice = self._preparse_assignment(node)
+        choice = self._clingo_parse_body_choice(unparsed_choice)
+        return HeadChoiceAssignment(
+            self.library,
+            self._location_from_node(node),
+            assigned_function,
+            choice.elements,
+        )
+    
 
-# def _parse_assignment_head(library: Library, code: str, assignment_pos: int) -> StatementAST:
+    def _location_from_node(self, node: Node) -> Location:
+        return Location(
+            Position(
+                self.library,
+                "<string>",
+                node.start_point.row+1,
+                node.start_point.column+1,
+                ),
+            Position(
+                self.library,
+                "<string>",
+                node.end_point.row+1,
+                node.end_point.column+1,
+                ),
+        )
 
+    def _clingo_parse_body(self, src: str) -> AST:
+        statement = ast.parse_statement(self.library, f":- {src}")
+        return statement.body
 
-# def _parse_assignment_rule(library: Library, rule: UnParsedAssignmentRule) -> StatementAST:
-#     statement = StatementAST()
-#     ast.parse_string(ast.Library(), rule.source, statement.append)
-#     return statement
+    def _clingo_parse_body_aggregate(self, src: str) -> AST:
+        body = self._clingo_parse_body(src + ".")
+        return body[0]
+
+    def _clingo_parse_body_choice(self, src: str) -> AST:
+        body = self._clingo_parse_body("#count" + src + ".")
+        return body[0]
+
+    def _tree_parse(self, src: bytes) -> Tree:
+        """
+        Parse source code into a Tree-sitter parse tree.
+        """
+        return self.parser.parse(src)
+
+    def _tree_parse_assignments(self, src: bytes) -> list[Node]:
+        """
+        Return all assignment_rule nodes in the parse tree.
+        """
+        root = self._tree_parse(src)
+        return TreeSitterParser._find_with_query(root, self.assignment_rule_query)
+
+    @staticmethod
+    def _find_with_query(root: Node, query: Query):
+        """
+        Return nodes of a given type using a simple query like '(<type>) @match'.
+        """
+        cursor = QueryCursor(query)
+
+        results = []
+        for capture_name, nodes in cursor.captures(root.root_node).items():
+            if capture_name == "match":
+                results.extend(nodes)
+        return results
+
+    
+# t = TreeSitterParser()
+# tree = t.parse("a := 1 :- b.")
+# print(tree.root_node)
--- tests/parsing/test_parser.py
+++ tests/parsing/test_parser.py
@@ -1,60 +1,200 @@
-from typing import Sequence
+import textwrap
+from typing import Any, Iterable, Sequence
 import unittest
 
+from attr import has
 from clingo import ast
 from clingo.core import Location, Position, Library
 
-from fasp.util.ast import SyntacticCheckVisitor, SyntacticError, TermAST
+from fasp.ast import AssignmentRule
+from fasp.util.ast import AST, SyntacticCheckVisitor, SyntacticError, TermAST
 from fasp.parsing import parser
+from tests.parsing.tree_sitter_util import format_ts_tree
+
 
 class TestParseAssignment(unittest.TestCase):
+
+    def _dispatch(self, ast1: Any, ast2: Any, whole1: AST, whole2: AST, parents1: list[AST] | None = None, parents2: list[AST] | None = None) -> None:
+        if not isinstance(ast1, AST) or not isinstance(ast2, AST):
+            return
+        if parents1 is None:
+            parents1 = []
+        if parents2 is None:
+            parents2 = []
+        parents1.append((type(ast1), ast1))
+        parents2.append((type(ast2), ast2))
+        self.assertEqual(
+            type(ast1), type(ast2), 
+            textwrap.dedent(f"""\
+            Type mismatch in {str(whole1)} vs {str(whole2)}
+            '{str(ast1)}' vs '{str(ast2)}'
+            Parents1: {' > '.join(f'{t.__name__}[{str(a)}]' for t, a in parents1)}
+            Parents2: {' > '.join(f'{t.__name__}[{str(a)}]' for t, a in parents2)}
+            """
+            )
+        )
+        children = [m for m in dir(ast1) if not m.startswith("_")]
+        for child in children:
+            attr1 = getattr(ast1, child)
+            attr2 = getattr(ast2, child)
+            if callable(attr1) or callable(attr2):
+                continue
+            if isinstance(attr1, Iterable):
+                for a1, a2 in zip(attr1, attr2):
+                    self._dispatch(a1, a2, whole1, whole2, parents1, parents2)
+            else:
+                # print(f"Dispatching {child}: {attr1} vs {attr2}")
+                self._dispatch(attr1, attr2, whole1, whole2, parents1, parents2)
+        # self.assertEqual(str(ast1), str(ast2), f"String mismatch in {str(whole1)} vs {str(whole2)}:\n {str(ast1)} != {str(ast2)}")
+        self.assertEqual(ast1, ast2, f"AST mismatch in {str(whole1)} vs {str(whole2)}:\n {str(ast1)} != {str(ast2)}")
+        if hasattr(ast1, "location") and hasattr(ast2, "location"):
+            self.assertEqual(ast1.location, ast2.location, f"Location mismatch in {str(whole1)} vs {str(whole2)}:\n {str(ast1.location)} != {str(ast2.location)}")
+        parents1.pop()
+        parents2.pop()
+
+    def assertASTEqual(self, ast1, ast2):
+        self._dispatch(ast1, ast2, ast1, ast2)
+        self.assertEqual(ast1, ast2)
 
     def setUp(self):
         self.messages = []
         self.lib = Library(logger=lambda t, msg: self.messages.append((t, msg)))
+        self.parser = parser.TreeSitterParser(self.lib)
 
-    def tearDown(self):
-        self.messages = []
-        self.lib = None
 
-    def test_parse_assignment_right_hand_side(self):
-        code = "g(Y)"
-        right, pc, lt = parser._parse_assignment_right(self.lib, code)
-        self.assertEqual(str(right), "g(Y)")
-        self.assertIsInstance(right, TermAST)
-        self.assertEqual(pc, [])
-        self.assertEqual(lt, [])
-        code = "a"
-        right, pc, lt = parser._parse_assignment_right(self.lib, code)
-        self.assertEqual(str(right), "a")
-        self.assertIsInstance(right, TermAST)
-        self.assertEqual(pc, [])
-        self.assertEqual(lt, [])
-        code = "#sum { X: p(X), q(X) }"
-        right, pc, lt = parser._parse_assignment_right(self.lib, code)
-        self.assertEqual(str(right), "#sum { X: p(X), q(X) }")
-        self.assertIsInstance(right, ast.BodyAggregate)
-        self.assertEqual(pc, [])
-        self.assertEqual(lt, [])
-        code = "g(Y), h(Y)"
-        with self.assertRaises(ValueError) as cm:
-             parser._parse_assignment_right(self.lib, code)
+    def test_tree_parse(self):
+        code = textwrap.dedent("""\
+            a := 1 :- b.
+            b.
+            """)
+        code = bytes(code, "utf8")
+        tree = self.parser._tree_parse(code)
+        self.maxDiff = None
         self.assertEqual(
-            str(cm.exception), f"Expected exactly one term or aggregate in assignment right-hand side of an assignment, found {code}")
-        code = "X = Y"
-        with self.assertRaises(ValueError) as cm:
-             parser._parse_assignment_right(self.lib, code)
-        self.assertEqual(
-            str(cm.exception), f"Expected exactly one term or aggregate in assignment right-hand side of an assignment, found {code}"
-        )
-        self.assertEqual(len(self.messages), 0)
+            format_ts_tree(tree, code).strip(),
+            textwrap.dedent("""\
+                └── source_file
+                    ├── statement
+                    │   └── assignment_rule
+                    │       ├── simple_assignment
+                    │       │   ├── term
+                    │       │   │   └── function
+                    │       │   │       └── name: identifier = a
+                    │       │   ├── := = :=
+                    │       │   └── term
+                    │       │       └── number = 1
+                    │       ├── :- = :-
+                    │       └── body
+                    │           ├── body_literal
+                    │           │   └── symbolic_atom
+                    │           │       └── name: identifier = b
+                    │           └── . = .
+                    └── statement
+                        └── rule
+                            ├── head
+                            │   └── literal
+                            │       └── symbolic_atom
+                            │           └── name: identifier = b
+                            └── . = .""").strip())
 
-    def test_parse_assignment_right_hand_side_invalid_aggregate(self):
-        code = "#sum { X: p(X): q(X) }"
-        with self.assertRaises(ValueError) as cm:
-             parser._parse_assignment_right(self.lib, code)
-        self.assertEqual(
-            str(cm.exception), f"Expected exactly one term or aggregate in assignment right-hand side of an assignment, found {code}")
-        self.assertEqual(len(self.messages), 1)
-
+    def test_tree_parse_assignments(self):
+        code = textwrap.dedent("""\
+            a := 1 :- b.
+            b.
+            c := 2.
+            """)
+        code = bytes(code, "utf8")
+        nodes = self.parser._tree_parse_assignments(code)
+        self.assertEqual(len(nodes), 2)
+        self.assertEqual(nodes[0].start_point, (0, 0))
+        self.assertEqual(nodes[0].end_point, (0, 12))
+        self.assertEqual(nodes[1].start_point, (2, 0))
+        self.assertEqual(nodes[1].end_point, (2, 7))
         
+        formatted_nodes = list(map(lambda n: format_ts_tree(n, code), nodes))
+        self.assertEqual(
+            formatted_nodes[0].strip(),
+            textwrap.dedent("""\
+                └── assignment_rule
+                    ├── simple_assignment
+                    │   ├── term
+                    │   │   └── function
+                    │   │       └── name: identifier = a
+                    │   ├── := = :=
+                    │   └── term
+                    │       └── number = 1
+                    ├── :- = :-
+                    └── body
+                        ├── body_literal
+                        │   └── symbolic_atom
+                        │       └── name: identifier = b
+                        └── . = .
+                    """).strip())
+        
+
+    def helper_parse(self, code, expected_clingo):
+        clingo_rules = []
+        try:
+            rules = self.parser.parse(code)
+            ast.parse_string(self.lib, expected_clingo, clingo_rules.append)
+        except Exception as e:
+            self.fail(f"Clingo messages: {self.messages}")
+        string_rules = [c.strip() for c in code.strip().split("\n")]
+        self.assertEqual(list(map(str, rules)), string_rules)
+        assignment_rules = []
+        previous_line = None
+        previous_column = None
+        for rule in rules:
+            if previous_line is not None and previous_column is not None:
+                self.assertEqual(rule.location.begin.line, previous_line+1)
+                self.assertEqual(rule.location.begin.column, previous_column)
+                self.assertEqual(rule.location.end.line, rule.location.begin.line)
+                self.assertEqual(rule.location.end.column, rule.location.begin.column+len(str(rule)))
+            previous_line = rule.location.begin.line
+            previous_column = rule.location.begin.column
+            if isinstance(rule, AssignmentRule):
+                assignment_rules.append(rule)
+                continue
+            self.assertASTEqual(rules[1], clingo_rules[1])
+            
+        self.assertEqual(len(assignment_rules), 2)
+
+    def test_parse_simple_assignment(self):
+        code = textwrap.dedent("""\
+            a := 1 :- b.
+            b.
+            c := 2.
+            """)
+        expected_clingo = textwrap.dedent("""\
+
+            b.
+
+        """)
+        self.helper_parse(code, expected_clingo)
+    
+
+    def test_parse_aggregate_assignment(self):
+        code = textwrap.dedent("""\
+            a := 1 :- b.
+            b.
+            c := #sum{X: p(X); Y,X: q(X,Y)}.
+            """)
+        expected_clingo = textwrap.dedent("""\
+
+            b.
+
+        """)
+        self.helper_parse(code, expected_clingo)
+
+    def test_parse_choice_assignment(self):
+        code = textwrap.dedent("""\
+            a := 1 :- b.
+            b.
+            c in {X: p(X); Y,X: q(X,Y)}.
+            """)
+        expected_clingo = textwrap.dedent("""\
+
+            b.
+
+        """)
+        self.helper_parse(code, expected_clingo)
