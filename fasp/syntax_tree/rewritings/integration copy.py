67684492bfe381c54dee28e5346c78f70ed3a876
Jorge Fandinno <jorgefandinno@gmail.com>
2026-03-05 08:59:52 -0600

    temporary

---
+++ fasp/syntax_tree/rewritings/integration copy.py
@@ -0,0 +1,262 @@
+from enum import IntEnum, auto
+from typing import Iterable, cast
+
+from clingo import ast
+
+from fasp.syntax_tree._context import RewriteContext as FASPRewriteContext
+from fasp.syntax_tree._nodes import (
+    AssignmentRule,
+    FASP_Statement,
+)
+from fasp.syntax_tree.collectors import (
+    collect_evaluable_functions,
+)
+from fasp.syntax_tree.rewritings.aggregates import normalize_assignment_aggregates
+from fasp.syntax_tree.rewritings.negated_literals import (
+    rewrite_negated_body_literals_from_statements,
+)
+from fasp.syntax_tree.rewritings.protecting import (
+    protect_assignments,
+    protect_comparisons,
+    restore_assignments,
+    restore_comparisons,
+)
+from fasp.syntax_tree.rewritings.showf import rewrite_showf
+from fasp.syntax_tree.rewritings.some_assignments import (
+    transform_choice_some_to_choice_assignment,
+)
+from fasp.syntax_tree.rewritings.to_asp import (
+    NormalForm2PredicateTransformer,
+    functional_constraints,
+)
+from fasp.syntax_tree.rewritings.unnesting.rules import RuleRewriteTransformer
+from fasp.syntax_tree.types import SymbolSignature
+
+
+class PipelineStage(IntEnum):
+    SHOWF = auto()
+    REWRITE_CHOICE_SOME = auto()
+    NORMALIZE_ASSIGNMENT_AGGREGATES = auto()
+    PROTECT_ASSIGNMENTS = auto()
+    PROTECT_COMPARISONS = auto()
+    CLINGO_REWRITE = auto()
+    RESTORE_COMPARISONS = auto()
+    RESTORE_ASSIGNMENTS = auto()
+    NEGATED_LITERALS = auto()
+    UNNEST_FUNCTIONS = auto()
+    TO_ASP = auto()
+
+
+# class Statement:
+
+#     def __init__(self, original: FASP_Statement):
+#         self.original = original
+#         self.has_assignments = isinstance(original, AssignmentRule)
+#         self.rewritten: list[FASP_Statement] = []
+
+
+class FASPProgramTransformer:
+    def __init__(
+        self,
+        ctx: FASPRewriteContext,
+        statement_asts: Iterable[FASP_Statement],
+    ):
+        self.ctx = ctx
+        self.lib = self.ctx.lib
+        self.library = self.lib.library
+
+        self.statement_asts = statement_asts
+        self.prefix = self.ctx.prefix_function
+
+        self.evaluable_functions: set[SymbolSignature] = set()
+        self.pipeline = list(PipelineStage)
+
+        self.PIPELINE_IMPL = {
+            PipelineStage.SHOWF: self._showf_to_show_wrapper,
+            PipelineStage.REWRITE_CHOICE_SOME: self._rewrite_choice_some_wrapper,
+            PipelineStage.NORMALIZE_ASSIGNMENT_AGGREGATES: self._normalize_assignment_aggregates_wrapper,
+            PipelineStage.PROTECT_ASSIGNMENTS: self._protect_assignments_wrapper,
+            PipelineStage.PROTECT_COMPARISONS: self._protect_comparisons_wrapper,
+            PipelineStage.CLINGO_REWRITE: self._clingo_rewrite_wrapper,
+            PipelineStage.RESTORE_COMPARISONS: self._restore_comparisons_wrapper,
+            PipelineStage.RESTORE_ASSIGNMENTS: self._restore_assignments_wrapper,
+            PipelineStage.NEGATED_LITERALS: self._negated_literals_wrapper,
+            PipelineStage.UNNEST_FUNCTIONS: self._unnest_functions_wrapper,
+            PipelineStage.TO_ASP: self._to_asp_wrapper,
+        }
+
+    def log_info(
+        self, statements: Iterable[FASP_Statement], stage: PipelineStage
+    ) -> Iterable[FASP_Statement]:  # pragma: no cover
+        out = list(statements)
+        for s in out:
+            if hasattr(s, "head") and hasattr(s, "body"):
+                print(stage.name, s, type(s.head))
+        return out
+
+    def transform(
+        self,
+        *,
+        stop_at: PipelineStage = PipelineStage.TO_ASP,
+        LOG: bool = False,
+    ) -> Iterable[FASP_Statement]:
+        """
+        Parse the program string, collect variables,
+        then run the pipeline and return transformed statements.
+        """
+        parsed_statements = self.statement_asts
+
+        # start pipeline with parsed_statements
+        current: Iterable[FASP_Statement] = parsed_statements
+        for stage in self.pipeline:
+            current = list(self.PIPELINE_IMPL[stage](current))
+            if LOG:
+                self.log_info(current, stage)  # pragma: no cover
+            if stage >= stop_at:
+                break
+
+        return current
+
+    def _rewrite_choice_some_wrapper(
+        self, statements: Iterable[FASP_Statement]
+    ) -> Iterable[FASP_Statement]:
+        out = [
+            transform_choice_some_to_choice_assignment(self.library, stmt)
+            for stmt in statements
+        ]
+        return out
+
+    def _normalize_assignment_aggregates_wrapper(
+        self, statements: Iterable[FASP_Statement]
+    ) -> Iterable[FASP_Statement]:
+        out = [
+            normalize_assignment_aggregates(self.library, stmt) for stmt in statements
+        ]
+        return out
+
+    def _protect_assignments_wrapper(
+        self, statements: Iterable[FASP_Statement]
+    ) -> Iterable[FASP_Statement]:
+        return protect_assignments(self.ctx, statements)
+
+    def _protect_comparisons_wrapper(
+        self, statements: Iterable[FASP_Statement]
+    ) -> Iterable[FASP_Statement]:
+        return protect_comparisons(self.library, statements)
+
+    def _clingo_rewrite_wrapper(
+        self, statements: Iterable[FASP_Statement]
+    ) -> Iterable[FASP_Statement]:
+        ctx = self.ctx.ctx
+        self.ctx.lib.ignore_info = True
+        out = []
+        errors = []
+        for stmt in statements:
+            assert not isinstance(stmt, AssignmentRule)
+            try:
+                rewritten_list = ast.rewrite_statement(ctx, stmt)
+            except RuntimeError as e:
+                errors.append((stmt, e))
+                continue
+            if rewritten_list:
+                for new_stmt in rewritten_list:
+                    out.append(new_stmt)
+            else:
+                out.append(stmt)
+        self.ctx.lib.ignore_info = False
+        if errors:
+            raise RuntimeError("rewriting failed", errors)
+        return out
+
+    def _restore_comparisons_wrapper(
+        self, statements: Iterable[FASP_Statement]
+    ) -> Iterable[FASP_Statement]:
+
+        stmts = list(statements)
+
+        for stmt in stmts:
+            assert not isinstance(stmt, AssignmentRule)
+        result = restore_comparisons(self.library, cast(Iterable[ast.Statement], stmts))
+        return result
+
+    def _restore_assignments_wrapper(
+        self, statements: Iterable[FASP_Statement]
+    ) -> Iterable[FASP_Statement]:
+        return restore_assignments(
+            self.lib,
+            cast(Iterable[ast.Statement], statements),
+            self.ctx.prefix_function,
+        )
+
+    def _negated_literals_wrapper(
+        self, statements: Iterable[FASP_Statement]
+    ) -> Iterable[FASP_Statement]:
+        return rewrite_negated_body_literals_from_statements(
+            self.lib, cast(Iterable[ast.Statement], statements)
+        )
+
+    def _unnest_functions_wrapper(
+        self, statements: Iterable[FASP_Statement]
+    ) -> Iterable[FASP_Statement]:
+        stmts = list(statements)
+        stmts2 = list(stmts)
+        self.evaluable_functions = collect_evaluable_functions(stmts2)
+
+        transformer = RuleRewriteTransformer(self.library, self.evaluable_functions)
+        out: list[FASP_Statement] = []
+        for stmt in stmts:
+            unnested_statement = transformer.transform_rule(stmt)
+            out.append(
+                cast(FASP_Statement, unnested_statement) if unnested_statement else stmt
+            )
+        return out
+
+    def _to_asp_wrapper(
+        self, statements: Iterable[FASP_Statement]
+    ) -> Iterable[FASP_Statement]:
+        # Collect evaluable functions again
+        self.evaluable_functions = collect_evaluable_functions(statements)
+
+        to_asp_transformer = NormalForm2PredicateTransformer(
+            self.library, self.evaluable_functions, self.prefix
+        )
+        out: list[ast.Statement] = []
+        for stmt in statements:
+            out.append(to_asp_transformer.rewrite(stmt))
+        return out
+
+    def _showf_to_show_wrapper(
+        self, statements: Iterable[FASP_Statement]
+    ) -> Iterable[FASP_Statement]:
+        return [rewrite_showf(self.ctx, stmt) for stmt in statements]
+
+
+def transform_to_clingo_statements(
+    ctx: FASPRewriteContext,
+    statement_asts: Iterable[FASP_Statement],
+    *,
+    stop_at: PipelineStage = PipelineStage.TO_ASP,
+    LOG: bool = False,
+) -> Iterable[ast.Statement]:
+    """Create a FASPProgramTransformer, run transform() and return
+    an iterable of clingo.ast.Statement (ast.Statement).
+
+    This asserts that the final transformed statements are instances
+    of clingo.ast.Statement and returns them as a list.
+    """
+
+    transformer = FASPProgramTransformer(ctx, statement_asts)
+    transformed = transformer.transform(stop_at=stop_at, LOG=LOG)
+
+    out: list[ast.Statement] = []
+    for stmt in transformed:
+        assert isinstance(
+            stmt, ast.Statement
+        ), f"Expected clingo.ast.Statement, got {type(stmt)}"
+        out.append(stmt)
+    out.extend(
+        functional_constraints(
+            ctx.lib.library, transformer.evaluable_functions, ctx.prefix_function
+        )
+    )
+    return out
---
+++ tests/syntax_tree/rewritting/_test_integration.py
@@ -0,0 +1,497 @@
+import textwrap
+import unittest
+
+from fasp.util.ast import ELibrary
+from fasp.syntax_tree.parsing.parser import parse_string
+from fasp.syntax_tree.rewritings.integration import (
+    FASPProgramTransformer,
+    PipelineStage,
+)
+from fasp.syntax_tree._context import RewriteContext
+
+
+class TestFASPProgramTransformer(unittest.TestCase):
+    def setUp(self):
+        self.elib = ELibrary()
+        self.ctx = RewriteContext(self.elib, prefix_function="F")
+        self.maxDiff = None  # Show full diff on assertion failure
+
+    def assertTransformEqual(
+        self,
+        program: str,
+        expected_program: str | None,
+        *,
+        test_pipeline=PipelineStage.TO_ASP,
+        LOG: bool = False,
+    ):
+        program = textwrap.dedent(program).strip()
+        expected_program = (
+            textwrap.dedent(expected_program).strip()
+            if expected_program is not None
+            else None
+        )
+
+        statement_asts = parse_string(self.elib, program)
+        transformer = FASPProgramTransformer(self.ctx, statement_asts)
+        transformed = transformer.transform(stop_at=test_pipeline, LOG=LOG)
+
+        transformed_str = "\n".join(
+            [str(statement).strip() for statement in transformed][1:]
+        )
+
+        self.assertEqual(transformed_str, expected_program)
+
+    def test_choice_some_rewrite(self):
+        self.assertTransformEqual(
+            "a := #some{X: p(X)} :- q(X), r.",
+            "{ a := X: p(X) } = 1 :- #count { X: p(X) } >= 1; q(X); r.",
+            test_pipeline=PipelineStage.NORMALIZE_ASSIGNMENT_AGGREGATES,
+        )
+
+    def test_assignment_aggregate_rewrite(self):
+        """
+        Test rewriting of assignment aggregates:
+        """
+        self.assertTransformEqual(
+            "f(X) := #sum { Y: p(Y,Z) , q(X), r(X) } :- b(X,Z).",
+            "f(X) := W :- b(X,Z); W = #sum { Y: p(Y,Z), q(X), r(X) }.",
+            test_pipeline=PipelineStage.NORMALIZE_ASSIGNMENT_AGGREGATES,
+        )
+
+    def test_combined_rewrite(self):
+        """
+        Combined program containing both a #some assignment and an assignment aggregate.
+        Verifies pipeline chaining.
+        """
+        self.assertTransformEqual(
+            """\
+                a := #some{X: p(X)} :- s.
+                f(X) := #count { Y: p(Y,Z) } :- b(X,Z).
+            """,
+            """\
+                { ASS(a,X): p(X) } = 1 :- #count { X: p(X) } >= 1; s.
+                ASS(f(X),W) :- b(X,Z); W = #count { Y: p(Y,Z) }.
+            """,
+            test_pipeline=PipelineStage.PROTECT_COMPARISONS,
+        )
+
+    def test_choice_some_rewrite_context(self):
+        self.assertTransformEqual(
+            "a := #sum{X: p(X)} :- q(X), r.",
+            "ASS(a,W) :- q(X); r; W = #sum { X: p(X) }.",
+            test_pipeline=PipelineStage.CLINGO_REWRITE,
+        )
+
+    def test_pool_rewrite(self):
+        self.assertTransformEqual(
+            "f(1;2) := Y :- g(Y).",
+            "ASS(f(1;2),Y) :- g(Y).",
+            test_pipeline=PipelineStage.PROTECT_COMPARISONS,
+        )
+
+    def test_pool_rewrite_2(self):
+        self.assertTransformEqual(
+            "f(1;2) := Y :- g(Y).",
+            """\
+                ASS(f(1),Y) :- g(Y).
+                ASS(f(2),Y) :- g(Y).
+            """,
+            test_pipeline=PipelineStage.CLINGO_REWRITE,
+        )
+
+    def test_pool_restore_assignments(self):
+        self.assertTransformEqual(
+            "f(1;2) := Y :- g(Y).",
+            """\
+                f(1) := Y :- g(Y).
+                f(2) := Y :- g(Y).
+            """,
+            test_pipeline=PipelineStage.RESTORE_ASSIGNMENTS,
+        )
+
+    def test_comparison_rewrite(self):
+        self.assertTransformEqual(
+            "a=100.",
+            "a=100.",
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+    def test_to_asp(self):
+        self.assertTransformEqual(
+            "f(1) := Y :- g(Y).",
+            "Ff(1,Y) :- g(Y).",
+        )
+
+    def test_total_integration(self):
+        self.assertTransformEqual(
+            "1 { sk(X, Y)  : skis(Y) } 1 :- sks(X).",
+            "1 <= #count { 0,sk(X,Y): sk(X,Y): skis(Y) } <= 1 :- sks(X).",
+        )
+
+    def test_king(self):
+        self.assertTransformEqual(
+            """
+            country(france).
+            country(usa).
+            person(felipe).
+
+            {king(C,X) : person(X)}:- country(C).
+
+
+            :- king(C1,X); king(C2,X); C1!=C2.
+            """,
+            """
+            country(france).
+            country(usa).
+            person(felipe).
+            #count { 0,king(C,X): king(C,X): person(X) } :- country(C).
+            :- king(C1,X); king(C2,X); C1!=C2.
+            """,
+        )
+
+    def test_head_aggregate_assignment(self):
+        self.assertTransformEqual(
+            "{king(C) := X: person(X)}:- country(C).",
+            "#count{ 0,Fking(C,X): king(C) := X: person(X) } :- country(C).",
+            test_pipeline=PipelineStage.RESTORE_ASSIGNMENTS,
+        )
+
+    def test_head_aggregate_assignment2(self):
+        self.assertTransformEqual(
+            "{king(spain) := felipe}.",
+            "#count{ 0,Fking(spain,felipe): king(spain) := felipe }.",
+            test_pipeline=PipelineStage.RESTORE_ASSIGNMENTS,
+        )
+
+    def test_head_aggregate_assignment2(self):
+        self.assertTransformEqual(
+            "{king(spain) := felipe}.",
+            "#count { 0,Fking(spain,felipe): Fking(spain,felipe) }.",
+            test_pipeline=PipelineStage.TO_ASP,
+        )
+
+    def test_no_change(self):
+        self.assertTransformEqual(
+            "f(X) :- g(X).",
+            "f(X) :- g(X).",
+        )
+
+    def test_aggregate(self):
+        self.assertTransformEqual(
+            """
+            f(X) := Y :- p(X,Y).
+            a := #sum{Y: f(X) = Y} :- q(X), r.
+            """,
+            """
+            Ff(X,Y) :- p(X,Y).
+            Fa(W) :- q(X); r; W = #sum { Y: Ff(X,Y) }.
+            """,
+        )
+
+    def test_choice_count(self):
+        self.assertTransformEqual(
+            """
+            f(X) := Y :- p(X,Y).
+            #count { Y: p(X): f(X) = Y} :- q(X); r.
+            """,
+            """
+            Ff(X,Y) :- p(X,Y).
+            #count { Y: p(X): Ff(X,Y) } :- q(X); r.
+            """,
+        )
+
+    def test_choice_count_Ass(self):
+        self.assertTransformEqual(
+            """
+            f(X) := Y :- p(X,Y).
+            #count { Y: g(X) := Y: f(X) = Y} :- q(X); r.
+            """,
+            """
+            Ff(X,Y) :- p(X,Y).
+            #count { Y: Fg(X,Y): Ff(X,Y) } :- q(X); r.
+            """,
+        )
+
+    def test_to_asp_head_aggregate_assignment(self):
+        self.assertTransformEqual(
+            "#count { 0,ass(king(f(C)),X): king(g(C)) := h(X): person(e(X)); ass(king(f(C)),X): f(X): person(e(X)) } :- country(C).",
+            "#count { 0,ass(FUN,X): Fking(g(C),h(X)): person(e(X)), Fking(f(C),FUN); ass(FUN2,X): f(X): person(e(X)), Fking(f(C),FUN2) } :- country(C).",
+        )
+
+        self.assertTransformEqual(
+            "{king(C) := X: person(X)}:- country(C).",
+            "#count { 0,Fking(C,X): Fking(C,X): person(X) } :- country(C).",
+        )
+
+    def test_fibo(self):
+        self.assertTransformEqual(
+            "fibo(X) := Y :- number(X); X>1; fibo(X-1) + fibo(X-2)=Y.",
+            "fibo(X) := Y :- number(X); X>1; FUN+FUN2=Y; fibo(X-1)=FUN; fibo(X-2)=FUN2.",
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+        self.assertTransformEqual(
+            "fibo(X) := Y :- number(X); X>1; fibo(X-1) + fibo(X-2)=Y.",
+            "Ffibo(X,Y) :- number(X); X>1; FUN+FUN2=Y; Ffibo(X-1,FUN); Ffibo(X-2,FUN2).",
+            test_pipeline=PipelineStage.TO_ASP,
+        )
+
+    def test_fibo2(self):
+        self.assertTransformEqual(
+            "fibo(X) := fibo(X-1) + fibo(X-2) :- number(X); X>1.",
+            "fibo(X) := FUN+FUN2 :- number(X); X>1; fibo(X-1)=FUN; fibo(X-2)=FUN2.",
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+    def test_king0(self):
+        self.assertTransformEqual(
+            "{ f(X) := Y: p(X,Y) } = 1.",
+            "#count{ 0,Ff(X,Y): f(X) := Y: p(X,Y) } = 1.",
+            test_pipeline=PipelineStage.RESTORE_ASSIGNMENTS,
+        )
+
+    def test_basic_negated_literals(self):
+        self.assertTransformEqual(
+            "a :- p(C); not country(C).",
+            "a :- p(C); #false: country(C).",
+            test_pipeline=PipelineStage.NEGATED_LITERALS,
+        )
+
+    def test_basic_negated_literals2(self):
+        self.assertTransformEqual(
+            """
+            f(X) := 1 :- p(X).
+            a :- p(C); #false: country(f(C)).
+            """,
+            """
+            f(X) := 1 :- p(X).
+            a :- p(C); #false: country(FUN), f(C)=FUN.
+            """,
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+    def test_basic_negated_literals2b(self):
+        self.assertTransformEqual(
+            """
+            f(X) := 1 :- p(X).
+            a :- p(C); #false: country(f(b)).
+            """,
+            """
+            f(X) := 1 :- p(X).
+            a :- p(*); #false: country(FUN), f(b)=FUN.
+            """,
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+        self.assertTransformEqual(
+            """
+            b := 1 :- p(X).
+            a :- p(C); #false: country(f(b)).
+            """,
+            """
+            b := 1 :- p(X).
+            a :- p(*); #false: country(f(FUN)), b=FUN.
+            """,
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+        self.assertTransformEqual(
+            """
+            b := 1 :- p(X).
+            a :- p(C); #false: country(f(b,c)).
+            """,
+            """
+            b := 1 :- p(X).
+            a :- p(*); #false: country(f(FUN,c)), b=FUN.
+            """,
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+        self.assertTransformEqual(
+            "a :- p(C); #false: country(C).",
+            "a :- p(C); #false: country(C).",
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS
+        )
+
+    def test_basic_negated_literals3(self):
+        self.assertTransformEqual(
+            "a :- p(C); not country(C).",
+            "a :- p(C); #false: country(C).",
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+    def test_basic_negated_literals4(self):
+        self.assertTransformEqual(
+            """
+            a := 1 :- p(X).
+            a :- p(*); #false: country(a).
+            """,
+            """
+            a := 1 :- p(X).
+            a :- p(*); #false: country(FUN), a=FUN.
+            """,
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+    def test_negated_literals(self):
+        self.assertTransformEqual(
+            "{king(C) := X: person(X)}:- country(C).",
+            "#count { 0,ASS(king(C),X): ASS(king(C),X): person(X) } :- country(C).",
+            test_pipeline=PipelineStage.CLINGO_REWRITE,
+        )
+
+    def test_minimize(self):
+        self.assertTransformEqual(
+            """
+            a := 1 :- p(X).
+            #maximize { 1@0,f(X),a: p(X), f(a) }.
+            """,
+            """
+            a := 1 :- p(X).
+            :~ p(X); f(FUN2); a=FUN; a=FUN2. [-1@0,f(X),FUN]
+            """,
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+    def test_weak_constraint(self):
+        self.assertTransformEqual(
+            """
+            a := 1 :- p(X).
+            :~ p(X); f(FUN2); a=FUN; a=FUN2. [-1@0,f(X),FUN]
+            """,
+            """
+            a := 1 :- p(X).
+            :~ p(X); f(FUN2); a=FUN; a=FUN2. [-1@0,f(X),FUN]
+            """,
+            test_pipeline=PipelineStage.RESTORE_ASSIGNMENTS,
+            # Clingo Rewrite removes FUN variables.
+        )
+
+    def test_weak_constraint2(self):
+        self.assertTransformEqual(
+            """
+            a := 1 :- p(X).
+            :~ p(X); f(FUN2); a=FUN; a=FUN2. [-1@0,f(X),FUN]
+            """,
+            """
+            a := 1 :- p(X).
+            :~ p(X); f(FUN2); a=FUN; a=FUN2. [-1@0,f(X),FUN]
+            """,
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+    def test_family(self):
+        self.assertTransformEqual(
+            "orphan(X) :- person(X), not father(X)=_, not mother(X)=_.",
+            "orphan(X) :- person(X); #false: father(X)=_; #false: mother(X)=_.",
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+        self.assertTransformEqual(
+            "orphan(X) :- person(X), not father(X)=A, not mother(X)=A.",
+            "orphan(X) :- person(X); #false: father(X)=A; #false: mother(X)=A.",
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+    def test_family_right(self):
+        self.assertTransformEqual(
+            "person(X) :- father(X)=_.",
+            "person(X) :- father(X)=_.",
+            test_pipeline=PipelineStage.RESTORE_ASSIGNMENTS,
+        )
+
+    def test_family_left(self):
+        self.assertTransformEqual(
+            "person(Y) :- father(_)=Y.",
+            "person(Y) :- father(_)=Y.",
+            test_pipeline=PipelineStage.RESTORE_ASSIGNMENTS,
+        )
+
+    def test_family_full(self):
+        self.assertTransformEqual(
+            """
+            father(cain):=adam.
+            father(abel):=adam.
+            mother(cain):=eve.
+            mother(abel):=eve.
+
+            % person(father(X)).
+            % person(mother(X)).
+            person(Y) :- father(_)=Y.
+            person(Y) :- mother(_)=Y.
+            person(X) :- father(X)=_.
+            person(X) :- mother(X)=_.
+            % male(father(X)).
+            % female(mother(X)).
+            male(Y) :- father(_)=Y.
+            female(Y) :- mother(_)=Y.
+
+            orphan(X) :- person(X), not father(X)=_, not mother(X)=_.
+
+            n_orphan := #count{X : orphan(X)}.
+            """,
+            """
+            Ffather(cain,adam).
+            Ffather(abel,adam).
+            Fmother(cain,eve).
+            Fmother(abel,eve).
+            % person(mother(X)).
+            #program base.
+            person(Y) :- Ffather(_,Y).
+            person(Y) :- Fmother(_,Y).
+            person(X) :- Ffather(X,_).
+            person(X) :- Fmother(X,_).
+            % male(father(X)).
+            % female(mother(X)).
+            male(Y) :- Ffather(_,Y).
+            female(Y) :- Fmother(_,Y).
+            orphan(X) :- person(X); #false: Ffather(X,_); #false: Fmother(X,_).
+            Fn_orphan(W) :- W = #count { X: orphan(X) }.""",
+            test_pipeline=PipelineStage.TO_ASP,
+        )
+
+    def test_hamiltonian(self):
+        self.assertTransformEqual(
+            """
+            next(X) := #some{Y: edge(X,Y)} :- vertex(X).
+            """,
+            """
+            { next(X) := Y: edge(X,Y) } = 1 :- #count { Y: edge(X,Y) } >= 1; vertex(X).
+            """,
+            test_pipeline=PipelineStage.REWRITE_CHOICE_SOME,
+        )
+        self.assertTransformEqual(
+            """
+            next(X) := #some{Y: edge(X,Y)} :- vertex(X).
+            """,
+            """
+            #count{ 0,Fnext(X,Y): next(X) := Y: edge(X,Y) } = 1 :- vertex(X); #count { Y: edge(X,Y) } >= 1.
+            """,
+            test_pipeline=PipelineStage.RESTORE_ASSIGNMENTS,
+        )
+        self.assertTransformEqual(
+            """
+            next(X) := #some{Y: edge(X,Y)} :- vertex(X).
+            """,
+            """
+            #count{ 0,Fnext(X,Y): next(X) := Y: edge(X,Y) } = 1 :- vertex(X); #count { Y: edge(X,Y) } >= 1.
+            """,
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+    def test_min_distance(self):
+        self.assertTransformEqual(
+            """
+            dist(X,Y) := M+1 :- dist(X,Z)=M; edge(Z,Y); #false: edge(W,Y), dist(X,W)=N, N<M.
+            """,
+            """
+            dist(X,Y) := 1*M+1 :- dist(X,Z)=M; edge(Z,Y); #false: edge(W,Y), dist(X,W)=N, N<M.
+            """,
+            test_pipeline=PipelineStage.UNNEST_FUNCTIONS,
+        )
+
+    def test_unsafe(self):
+        with self.assertRaisesRegex(RuntimeError, r"\('rewriting failed', \[\(<clingo\.ast\.StatementRule object at 0x[0-9A-Fa-f]+>, RuntimeError\('rewriting failed'\)\)\]\)"):
+            self.assertTransformEqual(
+                """
+                p(X) :- q(Y).
+                """,
+                ""
+            )
