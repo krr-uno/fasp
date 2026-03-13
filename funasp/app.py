980aeda0f7171c0747e02ea60b4d53319b4144bd
Jorge Fandinno <jorgefandinno@gmail.com>
2026-03-12 23:25:02 -0500

    update

--- funasp/app.py
+++ funasp/app.py
@@ -11,19 +11,18 @@ from funasp.control import Control
 from funasp.solve import Model
 from funasp.util.ast import ELibrary, ParsingException
 
-LIBC = ctypes.CDLL(None)  # None = current process's libc
+LIBC_NAME: str | None = None
+try:
+    if sys.platform.startswith("win"):
+        LIBC_NAME = "msvcrt"
+except Exception as e: # pragma: no cover
+    pass
+LIBC = ctypes.CDLL(LIBC_NAME)  # None = current process's libc
 
 # Declare fflush prototype
 LIBC.fflush.argtypes = [ctypes.c_void_p]
 LIBC.fflush.restype = ctypes.c_int
 
-def c_flush_stdout() -> None:
-    is_windows = True
-    try:
-        is_windows = sys.platform.startswith("win")
-    except Exception as e: # pragma: no cover
-        pass
-    LIBC.fflush("msvcrt" if is_windows else None)  # Flush C's stdout
 
 class FaspApp(App):
     def __init__(self, library: ELibrary, clingo_options: Sequence[str]) -> None:
@@ -59,7 +58,7 @@ class FaspApp(App):
     def print_model(
         self, model: solve.Model, default_printer: Callable[[], None]
     ) -> None:
-        c_flush_stdout()
+        LIBC.fflush(None)  # Flush C's stdout
         sys.stdout.write(str(Model(model, self._prefix)))
         sys.stdout.write("\n")
         sys.stdout.flush()
