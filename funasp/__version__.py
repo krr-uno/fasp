9d23b2cb32abe0e9ff7bf3b58adac789a22bd41c
Jorge Fandinno <jorgefandinno@gmail.com>
2026-03-19 13:39:12 -0500

    deploy workflow

--- funasp/__version__.py
+++ funasp/__version__.py
@@ -1,6 +1,1 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("funasp")
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "0.0.0"
+__version__ = "0.0.1"
